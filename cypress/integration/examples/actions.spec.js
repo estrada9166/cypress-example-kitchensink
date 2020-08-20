bfff2e90bf03eea1f9f1d6e915300fd155198f86
estrada9166 <estrada9166@hotmail.com>
2020-08-20 15:12:49 -0500

    new-test

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -7,7 +7,8 @@ context('Actions', () => {
 
   // https://on.cypress.io/interacting-with-elements
 
-  it('.type() - type into a DOM element', () => {
+  it('.type() - type into a DOM element ℹ️🎉', () => {
+    throw new Error('a')
     // https://on.cypress.io/type
     cy.get('.action-email')
       .type('fake@email.com').should('have.value', 'fake@email.com')
